d5d44d1a232fbd7a86b926b83bc2701432e71904
SouvikChoudhury360 <72061687+SouvikChoudhury360@users.noreply.github.com>
2021-02-11 22:44:11 +0530

    Update template.cpp

--- template.cpp
+++ template.cpp
@@ -41,8 +41,10 @@ void solve (){
 
 //Driver Function
 int main() {
-ios_base::sync_with_stdio(false);
-cin.tie(NULL);
+//ios_base::sync_with_stdio(false);
+//cin.tie(NULL);
+ios::sync_with_stdio(0);
+cin.tie(0);
 int test ;
 cin>>test ;
 FOR(i,5,10){
