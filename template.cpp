8c82eb9c44b2a7a7881e5719e99d8dfb35fd683f
Archishman Sengupta <71402528+ArchishmanSengupta@users.noreply.github.com>
2021-02-11 22:36:39 +0530

    updated boilerplate

--- template.cpp
+++ template.cpp
@@ -7,8 +7,20 @@
 #define forRL(i,a,b) for(i=a;i>=b;i--)
 #define INF 1e18
 #define MOD 1000000007
+#define PI 3.1415926535897932384626
+#define gc getchar_unlocked
 #define pb push_back
 #define input_ARR(arr,n) for(int i=0 ; i<n ;i++) {cin>>arr[i]}
+#define pb push_back
+#define mp make_pair
+#define F first
+#define S second
+#define all(x) x.begin(), x.end()
+#define rall(x) x.rbegin(), x.rend()
+#define clr(x) memset(x, 0, sizeof(x))
+#define sortall(x) sort(all(x))
+#define tr(it, x) for(auto it = x.begin(); it != x.end(); it++)
+#define trr(it, x) for(auto it = x.rbegin(); it != x.rend(); it+)
 
 
 using namespace std;
@@ -18,15 +30,16 @@ using namespace std;
 
 
 
+/* ========== YOUR CODE HERE ========= */
 
 void solve (){
     cout<<"HELLO WORLD" ;
 }
 
+/* ========== YOUR CODE HERE ========= */
 
 
-
-
+//Driver Function
 int main() {
 ios_base::sync_with_stdio(false);
 cin.tie(NULL);
