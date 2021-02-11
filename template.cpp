e66e2772a19bc73ff499446c87633bc8872b932c
Arin Chatterjee <68501026+rollexxx23@users.noreply.github.com>
2021-02-11 21:48:06 +0530

    Create template.cpp

---
+++ template.cpp
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+
+#define ll long long 
+#define vl vector <ll> 
+#define ull unsigned long long
+#define FOR(i,a,b) for(int i=a ; i<b ; i++)
+#define forRL(i,a,b) for(i=a;i>=b;i--)
+#define INF 1e18
+#define MOD 1000000007
+#define pb push_back
+#define input_ARR(arr,n) for(int i=0 ; i<n ;i++) {cin>>arr[i]}
+
+
+using namespace std;
+
+
+
+
+
+
+
+void solve (){
+    cout<<"HELLO WORLD" ;
+}
+
+
+
+
+
+int main() {
+ios_base::sync_with_stdio(false);
+cin.tie(NULL);
+int test ;
+cin>>test ;
+FOR(i,5,10){
+    cout<<"fhf" ;
+}
+while(test--) {
+solve() ;
+}
+
+return 0 ;
+}
