e8075736462b80ae39cafac7a98071d158ef8796
Archishman Sengupta <71402528+ArchishmanSengupta@users.noreply.github.com>
2021-04-20 00:51:50 +0530

    Updated Template

--- template.cpp
+++ template.cpp
@@ -23,37 +23,43 @@
 ~~~~~~~~~~~~~~~~~~~~~~*/  
 #include <bits/stdc++.h>
 #include <ext/pb_ds/assoc_container.hpp>
-#define ll long long 
-#define vl vector <ll> 
-#define pii pair<int,int>
-#define ull unsigned long long
-#define mii map<int,int>
-#define pqb priority_queue<int>
-#define FOR(i,a,b) for(int i=a ; i<b ; i++)
-#define forRL(i,a,b) for(i=a;i>=b;i--)
-#define INF 1e18
-#define MOD 1000000007
-#define PI 3.1415926535897932384626
-#define gc getchar_unlocked
-#define input_ARR(arr,n) for(int i=0 ; i<n ;i++) {cin>>arr[i]}
-// find if a given value is present in a container. Container version. Runs in log(n) for set and map
-#define present(c,x) ((c).find(x) != (c).end())
-//find version works for all containers. This is present in std namespace.
-#define cpresent(c,x) (find(all(c),x) != (c).end())
-// Avoiding wrap around of size()-1 where size is a unsigned int.
-#define sz(a) int((a).size())
+#define ll                                              long long 
+#define vl                                              vector <ll> 
+#define pii                                             pair<int,int>
+#define ull 						unsigned long long
+#define mii 						map<int,int>
+#define pqb 						priority_queue<int>
+#define FOR(i,a,b) 					for(int i=a ; i<b ; i++)
+#define forRL(i,a,b) 					for(i=a;i>=b;i--)
+#define INF 						1e18
+#define MOD 						1e9+7
+#define PI 						3.1415926535897932384626
+#define gc 						getchar_unlocked
+#define input_ARR(arr,n) 				for(int i=0 ; i<n ;i++) {cin>>arr[i]}
+// find if a given value is present in a container.
+//Container version. Runs in log(n) for set and map
+#define present(c,x) 					((c).find(x) != (c).end())
+//find version works for all containers. 
+//This is present in std namespace.
+#define cpresent(c,x) 					(find(all(c),x) != (c).end())
+// Avoiding wrap around of size()-1
+//where size is a unsigned int.
+#define sz(a) 						int((a).size())
 //Short hand for usual tokens
-#define pb push_back
-#define mp make_pair
-#define ff first
-#define ss second
-// to be used with algorithms that processes a container Eg: find(all(c),42)
-#define all(x) x.begin(), x.end() //Forward Traversal
-#define rall(x) x.rbegin(), x.rend() //Reverse Traversal
-#define clr(x) memset(x, 0, sizeof(x))
-#define sortall(x) sort(all(x))
-#define tr(it, x) for(auto it = x.begin(); it != x.end(); it++)
-#define trr(it, x) for(auto it = x.rbegin(); it != x.rend(); it+)
+#define pb						push_back
+#define mp 						make_pair
+#define ff 						first
+#define ss 						second
+// to be used with algorithms that processes
+//a container Eg: find(all(c),42)
+#define all(x) 						x.begin(), x.end() //Forward Traversal
+#define rall(x) 					x.rbegin(), x.rend() //Reverse Traversal
+#define clr(x) 						memset(x, 0, sizeof(x))
+#define sortall(x) 					sort(all(x))
+#define tr(it, x) 					for(auto it = x.begin(); it != x.end(); it++)
+#define trr(it, x) 					for(auto it = x.rbegin(); it != x.rend(); it+)
+
+#define binod 						ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0)
 using namespace __gnu_pbds;
 using namespace std;
 typedef vector<int> vi;
@@ -76,7 +82,7 @@ const int N = 3e5, M = N;
 
 
 void solve (){
-    cout<<"I AM BATMAN " ;
+    cout<<"I AM BATMAN \n" ;
 }
 
 
@@ -86,8 +92,7 @@ void solve (){
 int main() {
 //ios_base::sync_with_stdio(false);
 //cin.tie(NULL);
-ios::sync_with_stdio(0);
-cin.tie(0),cout.tie(0);
+binod;
 srand(chrono::high_resolution_clock::now().time_since_epoch().count());
 int test ;
 cin>>test ;
